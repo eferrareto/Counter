ad655a1fdee3afb2efaef3f7690bc8c0fa97bc87
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-08-28 11:16:50 -0300

    Update count.js

--- count.js
+++ count.js
@@ -6,16 +6,23 @@ function diminuir(){
     document.getElementById('numero').innerHTML = cont
     if(cont < 0){
         document.body.style.color = 'red'
+    } else if (cont == 0){
+        document.body.style.color = 'black'
     }
 }
 function resetar(){
     cont=0
     document.getElementById('numero').innerHTML = criandoNumero
+    if(cont == 0){
+        document.body.style.color = 'black'
+    }
 }
 function acrescentar(){
     cont++
     document.getElementById('numero').innerHTML = cont
     if(cont > 0){
+        document.body.style.color = 'green'
+    } else if(cont == 0){
         document.body.style.color = 'black'
     }
 }
